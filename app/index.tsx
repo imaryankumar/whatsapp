285bb12f5330c866446be9e26607d07aa236c6b4
Aryan Kumar <aryan8910kumar@gmail.com>
2024-10-05 02:12:57 +0530

    Add: First Commit Native

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const index = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="font-bold text-xl bg-red-500">index</Text>
+    </View>
+  );
+};
+
+export default index;
